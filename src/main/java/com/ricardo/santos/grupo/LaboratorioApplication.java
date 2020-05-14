17e1e46f869dfe4372da2261206a6d9a6b5208db
ricardobonifacio <ricardosantosb@hotmail.com>
2020-05-13 22:42:03 -0300

    first commit

---
+++ src/main/java/com/ricardo/santos/grupo/LaboratorioApplication.java
@@ -0,0 +1,13 @@
+package com.ricardo.santos.grupo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LaboratorioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LaboratorioApplication.class, args);
+	}
+
+}
